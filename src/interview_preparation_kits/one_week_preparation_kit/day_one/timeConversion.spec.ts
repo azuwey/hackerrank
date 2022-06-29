42de7a64ba8228feb32ccc77530739ca3b224544
David Zarandi <david.zarandi@pm.me>
2022-06-29 20:02:47 +0200

    Fixed styling issues Added lint step to the Node.js CI

--- src/interview_preparation_kits/one_week_preparation_kit/day_one/timeConversion.spec.ts
+++ src/interview_preparation_kits/one_week_preparation_kit/day_one/timeConversion.spec.ts
@@ -1,15 +1,15 @@
 import timeConversion from './timeConversion'
 
 describe('Time Conversion', () => {
-    test('Calculates the military time for \'12:01:00PM\' and returns \'12:01:00\'', () => {
-        expect(timeConversion('12:01:00PM')).toBe('12:01:00')
-    })
+  test('Calculates the military time for \'12:01:00PM\' and returns \'12:01:00\'', () => {
+    expect(timeConversion('12:01:00PM')).toBe('12:01:00')
+  })
 
-    test('Calculates the military time for \'12:01:00AM\' and returns \'00:01:00\'', () => {
-        expect(timeConversion('12:01:00AM')).toBe('00:01:00')
-    })
+  test('Calculates the military time for \'12:01:00AM\' and returns \'00:01:00\'', () => {
+    expect(timeConversion('12:01:00AM')).toBe('00:01:00')
+  })
 
-    test('Calculates the military time for \'07:05:45PM\' and returns \'19:05:45\'', () => {
-        expect(timeConversion('07:05:45PM')).toBe('19:05:45')
-    })
+  test('Calculates the military time for \'07:05:45PM\' and returns \'19:05:45\'', () => {
+    expect(timeConversion('07:05:45PM')).toBe('19:05:45')
+  })
 })
--- src/interview_preparation_kits/one_week_preparation_kit/day_one/timeConversion.ts
+++ src/interview_preparation_kits/one_week_preparation_kit/day_one/timeConversion.ts
@@ -1,16 +1,16 @@
-export default function timeConversion(time: string): string {
-    const [hour, minutes, secondsWithPeriod] = time.split(':')
-    const seconds = secondsWithPeriod.slice(0, 2)
-    const period = secondsWithPeriod.slice(2, 5)
-    let militaryHour = 0
+export default function timeConversion (time: string): string {
+  const [hour, minutes, secondsWithPeriod] = time.split(':')
+  const seconds = secondsWithPeriod.slice(0, 2)
+  const period = secondsWithPeriod.slice(2, 5)
+  let militaryHour = 0
 
-    if (period === 'AM' && hour !== '12') {
-        militaryHour = parseInt(hour)
-    } else if (period === 'PM' && hour !== '12') {
-        militaryHour = 12 + parseInt(hour)
-    } else if (period === 'PM' && hour === '12') {
-        militaryHour = 12
-    }
+  if (period === 'AM' && hour !== '12') {
+    militaryHour = parseInt(hour)
+  } else if (period === 'PM' && hour !== '12') {
+    militaryHour = 12 + parseInt(hour)
+  } else if (period === 'PM' && hour === '12') {
+    militaryHour = 12
+  }
 
-    return `${militaryHour.toString().padStart(2, '0')}:${minutes}:${seconds}`
+  return `${militaryHour.toString().padStart(2, '0')}:${minutes}:${seconds}`
 }
